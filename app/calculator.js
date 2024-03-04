b0fc85aa86e6d12b485f1652bce4e544444667f4
Kenneth M. Piekarski <kenneth@kmpiekarski.com>
2024-03-04 14:49:30 -0500

    Prettified.

--- app/calculator.js
+++ app/calculator.js
@@ -1,63 +1,71 @@
 // Constants for royalty rates. Adjust these as needed.
-    
-const umawTotalRate = 0.0056;
-const umawMonthlyCap = 290000;
-const spotifyTotalRate = 0.0051446772;
-const spotifyMonthlyCap = 1000;
-  
+
+const umawTotalRate = 0.0056
+const umawMonthlyCap = 290000
+const spotifyTotalRate = 0.0051446772
+const spotifyMonthlyCap = 1000
+
 // Calculate royalties for each type
 const spotifyPayout = (plays) => {
-  let payout = null;
-  payout = plays >= spotifyMonthlyCap ? plays * spotifyTotalRate : 0;
-  return payout;
+  let payout = null
+  payout = plays >= spotifyMonthlyCap ? plays * spotifyTotalRate : 0
+  return payout
 }
 const umawPayout = (plays) => {
-
-  console.log('plays: ' + plays);
-  let payout = null;       
-  payout = umawTotalRate * plays;
-  return payout;
+  console.log('plays: ' + plays)
+  let payout = null
+  payout = umawTotalRate * plays
+  return payout
 }
 
-document.getElementById('plays').addEventListener('input', function() {
-  
-  let value = this.value.replace(/[^0-9]/g, '');
-  
+document.getElementById('plays').addEventListener('input', function () {
+  let value = this.value.replace(/[^0-9]/g, '')
+
   // Convert the number string back to a number, then to a locale string to add commas
-  let realValue = parseFloat(value);
-  this.value = realValue.toLocaleString('en-US', { maximumFractionDigits: 0 });
-  
+  let realValue = parseFloat(value)
+  this.value = realValue.toLocaleString('en-US', { maximumFractionDigits: 0 })
+
   // If the input starts with a comma (e.g., ",234"), remove it
   if (this.value.charAt(0) === ',') {
-    this.value = this.value.substr(1);
-  }        
-  
+    this.value = this.value.substr(1)
+  }
+
   // If the initial value was empty or zero, it'll become 'NaN', so clear it
   if (this.value === 'NaN') {
-    this.value = '';
-    return;
+    this.value = ''
+    return
   }
-  
+
   // Restrict user input
   if (realValue > 99999999999) {
-    console.log('The maximum value is 99999999999.');
-    realValue = 99999999999;
-    this.value = realValue.toLocaleString('en-US', { maximumFractionDigits: 0 });
-    
+    console.log('The maximum value is 99999999999.')
+    realValue = 99999999999
+    this.value = realValue.toLocaleString('en-US', { maximumFractionDigits: 0 })
   }
-  
-  const plays = realValue;
-  
-  let pdecimals = 0;
-  if(plays < 100000)
-    pdecimals = 2;
-  
-  const spotifyTotal = spotifyPayout(plays);
-  const umawTotal = Math.min(umawPayout(plays), umawMonthlyCap);
-  const total = spotifyTotal - umawTotal;
+
+  const plays = realValue
+
+  let pdecimals = 0
+  if (plays < 100000) pdecimals = 2
+
+  const spotifyTotal = spotifyPayout(plays)
+  const umawTotal = Math.min(umawPayout(plays), umawMonthlyCap)
+  const total = spotifyTotal - umawTotal
 
   // Format the royalty values as currency
-  
-  document.getElementById('spotify-total-result').innerHTML = spotifyTotal.toLocaleString('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: pdecimals, maximumFractionDigits: pdecimals });  
-  document.getElementById('umaw-total-result').innerHTML = umawTotal.toLocaleString('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: pdecimals, maximumFractionDigits: pdecimals });
-});
+
+  document.getElementById('spotify-total-result').innerHTML =
+    spotifyTotal.toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: pdecimals,
+      maximumFractionDigits: pdecimals,
+    })
+  document.getElementById('umaw-total-result').innerHTML =
+    umawTotal.toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: pdecimals,
+      maximumFractionDigits: pdecimals,
+    })
+})
--- app/page.tsx
+++ app/page.tsx
@@ -1,58 +1,57 @@
-"use client"; // This is a client component
+'use client' // This is a client component
 
-import { useState } from 'react';
-import Image from "next/image";
+import { useState } from 'react'
+import Image from 'next/image'
 
 export default function Form() {
-
   // Constants for royalty rates. Adjust these as needed.
-  const [value, setValue] = useState('');
-  const umawTotalRate = 0.0056;
-  const umawMonthlyCap = 290000;
-  const spotifyTotalRate = 0.0051446772;
-  const spotifyAnnualCap = 1000;
-  
-  const plays = Number(value)  
+  const [value, setValue] = useState('')
+  const umawTotalRate = 0.0056
+  const umawMonthlyCap = 290000
+  const spotifyTotalRate = 0.0051446772
+  const spotifyAnnualCap = 1000
+
+  const plays = Number(value)
 
   // Calculate royalties for each type
   const spotifyRoyalty = (x: number) => {
-    let payout:number = 0;
-    payout = x >= spotifyAnnualCap ? x * spotifyTotalRate : 0;
-    return payout;
+    let payout: number = 0
+    payout = x >= spotifyAnnualCap ? x * spotifyTotalRate : 0
+    return payout
   }
 
   const umawRoyalty = (x: number) => {
-    let payout:number = 0;
-    payout = umawTotalRate * x;
-    return payout;
+    let payout: number = 0
+    payout = umawTotalRate * x
+    return payout
   }
 
-  const spotifyTotal:any = spotifyRoyalty(plays).toFixed(2);
-  const umawTotal:any = Math.min(umawRoyalty(plays), umawMonthlyCap).toFixed(2);
-  const difference:any = (umawTotal-spotifyTotal).toFixed(2);;
+  const spotifyTotal: any = spotifyRoyalty(plays).toFixed(2)
+  const umawTotal: any = Math.min(umawRoyalty(plays), umawMonthlyCap).toFixed(2)
+  const difference: any = (umawTotal - spotifyTotal).toFixed(2)
 
-  
   return (
     <main className="text-white text-center font-serif p-10 drop-shadow">
-      <div className="text-3xl pt-2 pb-2 outline-blue-500">#MakeStreamingPay Royalty Calculator</div>
+      <div className="text-3xl pt-2 pb-2 outline-blue-500">
+        #MakeStreamingPay Royalty Calculator
+      </div>
       <div className="grid grid-cols-1 content-center">
-
-        <label className="text-2xl pt-4 pb-4 italic">Your Number of Streams:</label>
-          <input
-            className="rounded-xl text-2xl p-3 mb-6 text-black w-full border-4 border-gray-50 focus:border-gray-50"
-            placeholder="100000???"
-            type="number"
-            max="11"
-            value={value}
-            onChange={e => setValue(e.target.value)}
-            onKeyPress={(event) => {
-              if (!/[0-9]/.test(event.key)) {
-                event.preventDefault();
-              }
-            }}
-          />
-        
-
+        <label className="text-2xl pt-4 pb-4 italic">
+          Your Number of Streams:
+        </label>
+        <input
+          className="rounded-xl text-2xl p-3 mb-6 text-black w-full border-4 border-gray-50 focus:border-gray-50"
+          placeholder="100000???"
+          type="number"
+          max="11"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          onKeyPress={(event) => {
+            if (!/[0-9]/.test(event.key)) {
+              event.preventDefault()
+            }
+          }}
+        />
       </div>
 
       <div className="grid grid-cols-2 gap-4 content-center">
@@ -89,12 +88,14 @@ export default function Form() {
         </div>
       </div>
 
-
       <div className="grid grid-cols-1 content-center mt-10">
         <div className="card-title">Difference:</div>
         <div className="text-3xl">${difference}</div>
-        <div className="text-sm mt-10 italic font-sans border-orange-200 border-2 p-5 rounded">Disclosure: due to special arrangements between Streaming services, DSPs and Labels, this is an estimate of current payout per stream.</div>
+        <div className="text-sm mt-10 italic font-sans border-orange-200 border-2 p-5 rounded">
+          Disclosure: due to special arrangements between Streaming services,
+          DSPs and Labels, this is an estimate of current payout per stream.
+        </div>
       </div>
     </main>
-  );
+  )
 }
