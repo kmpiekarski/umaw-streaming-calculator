e5cf06f330879fb98920c9d85b1eed8c5ecb8cd2
Kenneth M. Piekarski <kenneth@kmpiekarski.com>
2024-03-05 00:27:02 -0500

    Prevents zoom on mobile devices

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
@@ -8,6 +8,13 @@ export const metadata: Metadata = {
   title: 'Living Wage for Musicians Act Calculator',
   description:
     'Created by Kenneth M. Piekarski for United Musicians and Allied Workers (UMAW)',
+}
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 }
 
 export default function RootLayout({
